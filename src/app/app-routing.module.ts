9400243cdfaa381932b1dbdba13f9ba95f41763b
Wildo Enrique Ruiz González <wildo1997_@hotmail.com>
2021-04-19 12:54:22 -0400

    Adding routes and redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PermisoFormComponent } from './permiso-form/permiso-form.component';
+import { PermisosListComponent } from './permisos-list/permisos-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/permisos', pathMatch: 'full' },
+  { path: 'permisos', component: PermisosListComponent },
+  { path: 'permisos/new', component: PermisoFormComponent },
+  { path: 'permisos/:id', component: PermisoFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
